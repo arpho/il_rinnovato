77979332b7b90c4e5396c8d30e8df3efb98eb0d3
Unknown <damicogiuseppe77@gmail.com>
2018-08-04 00:22:45 +0200

    localizzazione
    datePicker è in italiano

--- src/components/dynamic-form/components/dynamic-form/dynamic-form-question.component.ts
+++ src/components/dynamic-form/components/dynamic-form/dynamic-form-question.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input,OnInit } from '@angular/core';
-import { FormGroup }        from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
-import { QuestionBase }     from '../../model/question-base';
+import { QuestionBase } from '../../model/question-base';
 
 @Component({
   selector: 'app-question',
@@ -10,9 +10,21 @@ import { QuestionBase }     from '../../model/question-base';
 export class DynamicFormQuestionComponent implements OnInit {
   @Input() question: QuestionBase<any>;
   @Input() form: FormGroup;
-  get isValid() { 
-    return this.form.controls[this.question.key].valid; }
-  ngOnInit(){
+  dayNames = [
+    "Domenica",
+    "Lunedì",
+    "Martedì",
+    "Mercoledì",
+    "Giovedì",
+    "Venerdì",
+    "Sabato"
+  ]
+  doneText = "Seleziona";
+  cancelText = "Annulla";
+  get isValid() {
+    return this.form.controls[this.question.key].valid;
+  }
+  ngOnInit() {
     console.log(this.question);
   }
 }
