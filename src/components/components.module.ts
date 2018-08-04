af9d8bde1a43507e158257bb44387ae1ca455d15
Unknown <damicogiuseppe77@gmail.com>
2018-08-04 12:24:45 +0200

    bug fixing
    removed double declaration of login component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { LoginComponent } from './login/login';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form';
 import { ProfileComponent } from './profile/profile';
 @NgModule({
-	declarations: [LoginComponent,
+	declarations: [,
     DynamicFormComponent,
     ProfileComponent],
 	imports: [],
-	exports: [LoginComponent,
+	exports: [
     DynamicFormComponent,
     ProfileComponent]
 })
